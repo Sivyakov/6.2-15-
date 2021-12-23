0eb14f5abe458848dd6e3bc86fa392f894c55f18
сивяков никит <reus-kit@mail.ru>
2021-12-23 10:56:19 +0600

    Добавьте файлы проекта.

---
+++ 6.2 15в/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _6._2_15в
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //6.2 базовый уровень 15v
+
+            int[,] mas = new int[7, 7];
+            Random rand = new Random();
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    mas[i, j] = rand.Next(123);
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+
+
+            }
+
+            int mainMax = ClassLibrary1.Class1.MaxElementMain(mas);
+            int secMax;
+            ClassLibrary1.Class1.MaxElementSecondary(mas, out secMax);
+            Console.WriteLine("Максимальный элемент главной диагонали = {0}", mainMax);
+            Console.WriteLine("Максимальный элемент побочной диагонали = {0}", secMax);
+            int p = mainMax * secMax;
+            Console.WriteLine("Произведение максимального элемента главной и побочной диагонали = {0}", p);
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int MaxElementMain(int[,] matrix)
+        {
+            int max = int.MinValue;
+            int n = (int)Math.Sqrt(matrix.Length);
+            for (int i = 0; i < n; i++)
+            {
+                if (matrix[i, i] > max)
+                {
+                    max = matrix[i, i];
+                }
+            }
+            return max;
+        }
+
+        public static void MaxElementSecondary(int[,] matrix, out int max)
+        {
+            max = int.MinValue;
+            int n = (int)Math.Sqrt(matrix.Length);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (matrix[n - i - 1, i] > max)
+                {
+                    max = matrix[n - i - 1, i];
+                }
+            }
+        }
+    }
+}
